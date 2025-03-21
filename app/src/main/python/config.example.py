bbf87c1f6b619bc8223bb94bf039e2e4124fa6f1
FeloSP8 <felipesrmd@gmail.com>
2025-03-21 20:48:29 +0100

    Add config example file

---
+++ app/src/main/python/config.example.py
@@ -0,0 +1,17 @@
+# Configuración del correo electrónico
+EMAIL_CONFIG = {
+    "remitente": "tu_email@gmail.com",
+    "contraseña": "tu_contraseña_app",
+    "destinatario": "destinatario@email.com",
+    "servidor_smtp": "smtp.gmail.com",
+    "puerto_smtp": 587,
+    "asunto": "WODs de la semana"
+}
+
+# Configuración para CrossFitDB
+CROSSFITDB_CONFIG = {
+    "username": "tu_usuario",
+    "password": "tu_contraseña",
+    "id_user": "tu_id",
+    "id_application": "id_app"
+}
